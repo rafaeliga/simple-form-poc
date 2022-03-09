877dd206478ea81c59d438bdb7006e8b50d91e0a
Rafael Iga <rafael.f.iga@gmail.com>
2022-03-09 11:31:44 -0300

    simple form wrapper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_132923) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_142719) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_132923) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: false
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
